f1469c290a7bbdc34e264db49f3684adbe76bcbd
Tom Hansen <tomh@uwm.edu>
2023-12-05 01:00:28 -0600

    yes-no-swipe UI working again

---
+++ cgi/user_chose.cgi
@@ -0,0 +1,45 @@
+#!/home/tomh/slbetrain/venv/bin/python3
+import cgitb
+import cgi
+import json
+import os
+import sys
+import time
+import datetime
+import glob
+import sqlite3
+
+
+cgitb.enable()
+
+print("Cache-Control: no-cache, no-store, must-revalidate")
+print("Pragma: no-cache")
+print("Expires: 0")
+
+print("Content-Type: application/json; charset=utf-8")
+print()
+# get values from the query string from an HTTP GET
+#
+# get the query string from an HTTP GET
+form = cgi.FieldStorage()
+
+recid = form.getvalue("recid", "")
+user = form.getvalue("user", "")
+choice = form.getvalue("choice", "")
+
+if recid == "" or choice == "":
+    print(json.dumps({'error':'missing parameters'}))
+    sys.exit(0)
+
+
+conn = sqlite3.connect('/home/tomh/slbetrain/slbeuser_tomh.sqlite3')
+c = conn.cursor()
+c.execute('''
+    INSERT INTO slbeuser_tomh
+       (recid, user, choice) VALUES (?,?,?)''', (recid, user, choice))
+conn.commit()
+conn.close()
+
+print(json.dumps({'success':'true'}))
+sys.exit(0)
+
--- index.js
+++ index.js
@@ -1,6 +1,54 @@
 
 const PAGESIZE = 8;
 
+// check if SSL is enabled; if not, redirect to SSL
+if (window.location.protocol != "https:") {
+	window.location.href = "https://"+window.location.host+window.location.pathname;
+}
+var user="tomh";
+// implement simple user auth: if there is a cookie, use it, otherwise prompt for a username
+// check if user/password authenticates with ldap server. if so, set cookie and proceed, otherwise prompt again
+// if user is authenticated, show the training page
+// if user is not authenticated, show the login page
+
+function login() {
+	user = $("input#user")[0].value;
+	pass = $("input#pass")[0].value;
+	$.ajax({
+		url: "/slbetrain/cgi-bin/login.cgi?user="+user+"&pass="+pass,
+		complete: function(data) {
+			console.log(data);
+			if (data.responseJSON['status'] == "ok") {
+				// set cookie
+				document.authtoken = data.responseJSON['token'];
+				document.cookie = "authuser="+user;
+				document.logintime = new Date();
+
+				// show training page
+				// updateBody();
+				$("div#login")[0].style.display = "none";
+				$("div#root")[0].style.display = "block";
+				$("div#root")[0].style.opacity = 1;
+				$("div#root")[0].style.zIndex = 1;
+				$("div#root")[0].style.position = "relative";
+				$("div#root")[0].style.top = 0;
+				$("div#root")[0].style.left = 0;
+			} else {
+				// show error
+				$("div#login")[0].style.display = "block";
+				$("div#root")[0].style.display = "none";
+				$("div#login")[0].style.opacity = 1;
+				$("div#login")[0].style.zIndex = 1;
+				$("div#login")[0].style.position = "relative";
+				$("div#login")[0].style.top = 0;
+				$("div#login")[0].style.left = 0;
+			}
+		}
+	});
+}
+
+
+
 function prev() {
 	window._x_startAt -= PAGESIZE;
 	if (window._x_startAt < 0) {
@@ -14,49 +62,58 @@ function next() {
 	updateBody();
 }
 
-function choseYes(recid) {
-	targ = $("div[data-recid='"+recid+"']")[0];
-	console.log('right');
-	console.log(targ);
-	$(targ).animate({left:"125%",opacity:0},500);
-	$(targ.parentNode).animate({opacity:0},500, function() {
-		console.log("removing "+recid);
-		console.log(targ);
-		targ.parentNode.remove();
+
+function chose(recid, yesno) {
+	console.log("chose "+recid+" "+yesno);
+	switch (yesno) {
+		case "yes":
+			leftAnim = "125%";
+			direction = "left";
+			break;
+		case "no":
+			leftAnim = "-125%";
+			direction = "right";
+			break;
+		default:
+			return;
+	}
+
+	var targ_imgflip = $("div.imgflip[data-recid="+recid+"]")[0];
+	var targ_trainbox = $("div.trainbox[data-recid="+recid+"]")[0];
+	var targ_btnstrip = $("div.btnstrip[data-recid="+recid+"]")[0];
+	console.log("about to animate "+direction);
+	$(targ_imgflip).animate({left:leftAnim,opacity:0},500);
+	console.log("about to animate fadeout");
+	$(targ_btnstrip).animate({opacity:0},500);
+	$(targ_trainbox).animate({opacity:0},500,function() {
+		console.log("completed the animation");
+		console.log("sending user choice");
+		$.ajax({
+			url: "/slbetrain/cgi-bin/user_chose.cgi?recid="+recid+"&user="+user+"&choice="+yesno,
+			complete: function(data) {
+				console.log("completed the processing of user choice");
+				console.log(data);
+				console.log("removing "+recid);
+				var targ_trainbox = $("div.trainbox[data-recid="+recid+"]")[0];
+				$(targ_trainbox).remove();
+				console.log("removed "+recid);
+			}
+		});
 	});
-
-	return;
-
-
-
-
-	$.ajax("/slbetrain/cgi-bin/chose.cgi?recid="+recid+"&yes=1", {
-		"complete": function(data) {
-			console.log(data);
-			updateBody();
-		}
-	});
-}
-
-function choseNo(recid) {
-	targ = $("div[data-recid='"+recid+"']")[0];
-	console.log('left');
-	console.log(targ);
-	$(targ).animate({left:"-125%",opacity:0},500);
-	$(targ.parentNode).animate({opacity:0},500, function() {
-		console.log("removing "+recid);
-		console.log(targ);
-		targ.parentNode.remove();
-	});
-	return;
-
-
-
-
-	$.ajax("/slbetrain/cgi-bin/chose.cgi?recid="+recid+"&no=1", {
-		"complete": function(data) {
-			console.log(data);
-			updateBody();
+	console.log("completed the animation");
+	console.log("sending user choice");
+	$.ajax({
+		url: "/slbetrain/cgi-bin/user_chose.cgi?recid="+recid+"&user="+user+"&choice="+yesno,
+		complete: function(data) {
+			window.setTimeout(function() {
+				console.log("completed the processing of user choice");
+				console.log(data);
+				console.log("removing "+recid);
+				var targ_trainbox = $("div.trainbox[data-recid="+recid+"]")[0];
+				$(targ_trainbox).remove();
+				console.log("removed "+recid);
+			}, 800);
+				//updateBody();
 		}
 	});
 }
@@ -92,31 +149,32 @@ function updateBody() {
 			for (var i=window._x_startAt; i<Math.min(jdata.length,window._x_startAt+PAGESIZE); i++) {
 				var d=bdy.appendChild(document.createElement("div"));
 				d.classList.add("trainbox");
+				d.setAttribute("data-recid",jdata[i]['recid']);
 				jdi = jdata[i][0]+'';
 				hiresjpg = jdi.replace(".gif",".jpg").replace("conts","jpg").replace("_sfs","_sf2");
-				d.innerHTML=('<div class="imgflip" data-recid="'+jdata[i]['recid']+'">'
-					+'<img style="z-index:1" class="sample" data-recid="'+jdata[i]['recid']+'" src="data:image/jpg;base64,'+jdata[i]['still_context']+'">'
-					+ '<img style="z-index:0" class="sampgif" data-recid="'+jdata[i]['recid']+'" src="data:image/gif;base64,'+jdata[i]['animated_context']+'"></div>'
-					+'<div class="btnstrip">'
-						+'<button class="train no" onclick="choseNo('+jdata[i]['recid']+')"><img src="fish_no.png"></button>'
-						+'<button class="train yes" onclick="choseYes('+jdata[i]['recid']+')"><img src="fish_yes.png"</button>'
-					+'</div>'
+				d.innerHTML=(`
+				<div class="imgflip" data-recid="${jdata[i]['recid']}">
+					<img style="z-index:1" class="sample" data-recid="${jdata[i]['recid']}" src="data:image/jpg;base64,${jdata[i]['still_context']}">
+					<img style="z-index:0" class="sampgif" data-recid="${jdata[i]['recid']}" src="data:image/gif;base64,${jdata[i]['animated_context']}">
+				</div>
+				<div class="btnstrip" data-recid="${jdata[i]['recid']}">
+					<button class="train no" onclick="chose(${jdata[i]['recid']},'no')"><img src="fish_no.png"></button>
+					<button class="train yes" onclick="chose(${jdata[i]['recid']},'yes')"><img src="fish_yes.png"</button>
+				</div>`
 				);
 			}
 			$("div.imgflip").swipe({
 				swipeLeft:function(e) {
 					console.log("swiped left!");
 					console.log(e);
-					choseNo(e.target.attributes['data-recid'].value);
-					//$(e.target.parentNode).animate({left:"-125%"},500);
-					//e.preventDefault();
+					chose(e.target.attributes['data-recid'].value, "no");
+					e.preventDefault();
 				},
 				swipeRight:function(e) {
 					console.log("swiped right!");
 					console.log(e);
-					choseYes(e.target.attributes['data-recid'].value);
-					//$(e.target.parentNode).animate({left:"125%"},500);
-					//e.preventDefault();
+					chose(e.target.attributes['data-recid'].value, "yes");
+					e.preventDefault();
 				}
 			});
 		}

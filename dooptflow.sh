6698542344ae2cc2171e788160a4b641a158ec3b
Tom Hansen <tomh@uwm.edu>
2023-12-05 15:51:11 -0600

    experiment gone a bimt awry.

--- dooptflow.sh
+++ dooptflow.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # This script is used to run the optical flow algorithm on a set of images
-
+LOCATIONCODE="SBLE"
 # Usage: ./dooptflow.sh <directory> <output directory> <output video name>
-rm -rf trains/SLBE*
-for mf in mp4/SLBE_*.mp4; do
+rm -rf trains/$LOCATIONCODE*
+for mf in mp4/$LOCATIONCODE_*.mp4; do
 	echo $mf
 	./optflow2.py $mf $1
 	./doffmpeg.sh $mf

4ead091700d64b7168c62a97d512f7f1bebc78d5
Tom Hansen <tomh@uwm.edu>
2024-03-29 11:29:06 -0500

    getting https working

--- server.py
+++ server.py
@@ -4,10 +4,16 @@ import http.server, ssl
 from ssl import SSLContext
 
 server_address = ('0.0.0.0', 4443)
-httpd = http.server.HTTPServer(server_address, http.server.CGIHTTPRequestHandler)
+
+handler = http.server.CGIHTTPRequestHandler
+
+print(handler.cgi_directories)
+
+
+httpd = http.server.ThreadingHTTPServer(server_address, http.server.CGIHTTPRequestHandler)
 
 context = SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.load_cert_chain('./ssl/certs/apache-selfsigned.crt', './ssl/private/apache-selfsigned.key')
+context.load_cert_chain('./ssl/selfsigned.crt', './ssl/selfsigned.key')
 
 httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
 
